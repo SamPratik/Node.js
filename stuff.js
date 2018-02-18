2e6b555138ab02cfaaef48faeb071b47b69facad
SamPratik <pratik.anwar@gmail.com>
2018-02-19 00:22:01 +0600

    Tutorial 7 - Module Patterns [Part 2 - sending multiple data from module]

--- stuff.js
+++ stuff.js
@@ -1,15 +1,12 @@
-var counter = function(arr) {
+// module.exports is a blank object, we are setting properties on this module.exports
+// object and sending data...
+
+module.exports.counter = function(arr) {
   return 'There are ' + arr.length + ' elements!';
 }
 
-var adder = function(a, b) {
+module.exports.adder = function(a, b) {
   return `The sum of '${a}' & '${b}' is: ${a+b}`;
 }
 
-var pi = 3.1416;
-
-// module.exports is a blank array, we are setting properties on this module.exports
-// object and sending data...
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
+module.exports.pi = 3.1416;
