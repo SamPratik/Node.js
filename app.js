7515f315cc462098f35c487e451fd0c7947c881d
SamPratik <pratik.anwar@gmail.com>
2018-02-20 00:20:08 +0600

    Tutorial 8 - The node event emitter [Part - 1]

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// we are assigning the module.exports object in stuff. So, stuff is now an
-// object and it has the same properties (counter, adder, pi) as module.exports
-// and we can access those properties to get the data sent from 'stuff.js'
-// module...
-var stuff = require('./stuff');
+// 'evnets' is a core module which is included in this module to call EventEmitter()
+// constructor...
+var events = require('events');
+var myEmitter = new events.EventEmitter();
 
-//accessing those properties to get the data from stuff.js module...
-console.log(stuff.counter(['Pratik', 'Affan', 'Vote']));
-console.log(stuff.adder(5, 6));
-console.log(stuff.adder(stuff.pi, 5));
+// EventEmitter() is used to listen to custom evnet...
+myEmitter.on('someEvent', function(msg) {
+  console.log(msg);
+});
+
+// EventEmitter() is used to fire custom event...
+myEmitter.emit('someEvent', 'This is a message!');
