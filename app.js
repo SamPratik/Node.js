3ff1d0d2ac7c944a0368c3dce63763c332af2017
SamPratik <pratik.anwar@gmail.com>
2018-02-20 13:46:21 +0600

    Tutorial 9 - Reading and Writting Files(fs) [Part 3 - readFile() assynchronous method]

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // module required to read and write file...
 var fs = require('fs');
 
-// readFileSync() is used to read a file synchronously(means it will not move to next
-// code until finishes reading the defined file) and 'utf8' is used to convert the
-// binary datas into real characters...
-var readme = fs.readFileSync('./readme.txt', 'utf8');
+// readFile() is a assynchronous function. it will not block the next code
+// from being executed while reading a file. in this case 'test' will be
+// logged first and then the content of 'readme.txt' will be logged...
 
-// writting synchronously the content of 'readme.txt' file to the 'writeme.txt' file... 
-fs.writeFileSync('./writeme.txt', readme);
+// raeadFile() has a third parameter which is a callback function and it will be
+// executed after reading the whole content of a file. This callback function
+// takes two parameters(err and data)...
+var readme = fs.readFile('./readme.txt', 'utf8', function(err, data) {
+  console.log(data);
+});
+
+// logging the a message to test the assynchronous method 'readFile()'...
+console.log('test');
