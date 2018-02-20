bde9a67aa446cdc9d81fb09cb9bf147f5a128e03
SamPratik <pratik.anwar@gmail.com>
2018-02-20 15:37:49 +0600

    Creating and Removing directories [Part 4 - creating directory assynchronously using mkdir()]

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var fs = require('fs');
 
-// removing a directory synchronously(that means it will block the next codes
-// from being executed until it removes the stuff folder)...
-fs.rmdirSync('./stuff');
+// assynchrounous mkdir() function is used to create stuff folder. then readFile()
+// is used to read 'readme.txt' file and then we have used assynchrounous witeFile()
+// to white the read content into 'stuff/writeme.txt'...
+fs.mkdir('./stuff', function() {
+  fs.readFile('./readme.txt', function(err, data) {
+    fs.writeFile('./stuff/writeme.txt', data, function(err) {
+      console.log('successfully written in the file!');
+    });
+  });
+});
