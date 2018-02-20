d0cda22701deb806eacba600c3b2905e88ae477f
SamPratik <pratik.anwar@gmail.com>
2018-02-20 14:40:19 +0600

    Tutorial 9 - Reading and Writting Files(fs) [Part 4 - writeFile() assynchronous method]

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 // module required to read and write file...
 var fs = require('fs');
 
-// readFile() is a assynchronous function. it will not block the next code
-// from being executed while reading a file. in this case 'test' will be
-// logged first and then the content of 'readme.txt' will be logged...
-
-// raeadFile() has a third parameter which is a callback function and it will be
-// executed after reading the whole content of a file. This callback function
-// takes two parameters(err and data)...
-var readme = fs.readFile('./readme.txt', 'utf8', function(err, data) {
-  console.log(data);
+// readFile() is used to read the specified file assynchronously. As this is a
+// assynchronous function so a callback is used to write the read data to write in
+// 'writeme.txt' file using whiteFile()...
+fs.readFile('./readme.txt', 'utf8', (err, data) => {
+  fs.writeFile('./writeme.txt', data, (err) => {
+    console.log(data);
+  });
 });
-
-// logging the a message to test the assynchronous method 'readFile()'...
-console.log('test');
