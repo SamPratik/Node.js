bb98d5cd3b93ec4da5c045348af9b02136739f2c
SamPratik <pratik.anwar@gmail.com>
2018-02-20 15:20:22 +0600

    Tutorial 10 - Creating and Removing directories [Part 1 - deleting file using unlink()]

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-// module required to read and write file...
 var fs = require('fs');
 
-// readFile() is used to read the specified file assynchronously. As this is a
-// assynchronous function so a callback is used to write the read data to write in
-// 'writeme.txt' file using whiteFile()...
-fs.readFile('./readme.txt', 'utf8', (err, data) => {
-  fs.writeFile('./writeme.txt', data, (err) => {
-    console.log(data);
-  });
-});
+// unlink() method deletes file
+fs.unlink('./deleteme.txt');
