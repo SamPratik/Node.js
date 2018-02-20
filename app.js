3f708472d3606bf449a9479de5a54048a3322e40
SamPratik <pratik.anwar@gmail.com>
2018-02-20 21:57:10 +0600

    Tutorial 16 - Pipes [Part 1 - writting to writeme.txt using pipe()]

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var fs = require('fs');
 
-// using createReadStream() to create a stream while reading the file.
-// in this process a little chunk of data will be passed to the client thorugh buffer
-// instead of the whole data. this will improve the performance cause the user
-// wont have to wait for the whole data to load...
-
-// creating a read stream...
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-// creating write stream so that the read chunk can be written to a file...
+// Reading content from readme.txt file chunk by chunk instead of reading the
+// whole content...
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt');
+// will write the content from readme.txt to writeme.txt chunk by chunk...
 var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
 
-// this will be fired when a piece of data chunk will be read...
-myReadStream.on('data', function(chunk) {
-  console.log('Cunk received:');
-  // writting the read chunk into 'writeme.txt'...
-  myWriteStream.write(chunk);
-})
+// pipe() will take all the heavy lifting for us. it will listen to the data event
+// which is fired after every chunk is read and then take that chunk and write that
+// chunk to the 'myWriteStream'...
+myReadStream.pipe(myWriteStream);
