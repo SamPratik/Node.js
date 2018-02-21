cdfdc4e1662931d3b2802fee52b78308c269f7f6
SamPratik <pratik.anwar@gmail.com>
2018-02-21 12:47:15 +0600

    Tutorial 19 - Basic Routing

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 var fs = require('fs');
 var http = require('http');
 
-// creating a server which will return a response...
+// creating server...
 var server = http.createServer(function(req, res) {
-  // req.url returns the path after the ip address and the port number...
-  console.log('The request path is: ' + req.url);
-  // setting the response header to JSON...
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var myObj = {
-    name: 'Ryu',
-    job: 'Ninja',
-    age: 23
-  };
-  // sending the myObj as response but we cannot send object, we can send string
-  // so we will convert the object as json...
-  res.end(JSON.stringify(myObj));
+  // printing the path after the IP address and the port number...
+  console.log('You have requested to: ' + req.url);
+  // matching the path with '/home' or '/' and returning index.html as response...
+  if(req.url === '/home' || req.url === '/') {
+    // setting the header and content type to html...
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    // reading index.html chunk by chunk...
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    // sending the read content as response to the client...
+    myReadStream.pipe(res);
+  }
+  else if(req.url === '/contact') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html', 'utf8').pipe(res);
+  } else if(req.url === '/api/friends') {
+    myObj = [
+      {name: 'Pratik', age: 23},
+      {name: 'Affan', age: 24},
+      {name: 'Turag', age: 22}
+    ]
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(myObj));
+  }
+  // if none of the path matches then we will return 404.html as response... 
+  else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/404.html', 'utf8');
+    myReadStream.pipe(res);
+  }
 });
 
-// now, if we request to 127.0.0.1:3000 we will get the content of index.html...
 server.listen(3000, '127.0.0.1');
+
 console.log('listening to port 3000:');
