532e7d530e3cf8e2ecd64cd84cabc006a19e15d7
SamPratik <pratik.anwar@gmail.com>
2018-02-21 22:24:41 +0600

    Tutorial 30 - Handling POST Requests

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 // when we have to show dynamic parameters in template we need to install 'ejs'\
 // package and have to set 'view engile' to 'ejs'.
@@ -12,6 +12,20 @@ app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact', {qs: req.query});
+})
+
+// this middleware will fire when a POST request will come to the route on which
+// this middleware is defined...
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// this route will be executed when the contact form will be submitted...
+app.post('/contact', urlencodedParser, function(req, res) {
+  // passing the submitted data as an object in 'contact-success'...
+  res.render('contact-success', {data: req.body});
 });
 
 app.get('*', function(req, res) {
